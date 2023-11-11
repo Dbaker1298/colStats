bdba1a232a1d2902ff4b5a0a03ad6cdd9a4f6dbc
David Baker <dpbaker1298@gmail.com>
2023-11-11 14:11:01 -0500

    Add function to run statistical operations on CSV files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -17,4 +18,53 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func run(filenames []string, op string, column int, out io.Writer) error {
+	var opFunc statsFunc
+
+	if len(filenames) == 0 {
+		return ErrNoFiles
+	}
+
+	if column < 1 {
+		return fmt.Errorf("%w: %d", ErrInvalidColumn, column)
+	}
+
+	// Validate the operation and define the opFunc accordingly
+	switch op {
+	case "sum":
+		opFunc = sum
+	case "avg":
+		opFunc = avg
+	default:
+		return fmt.Errorf("%w: %s", ErrInvalidOperation, op)
+	}
+
+	consolidate := make([]float64, 0)
+
+	// loop through all files adding their data to consolidate
+	for _, fname := range filenames {
+		// Open the file for reading
+		f, err := os.Open(fname)
+		if err != nil {
+			return fmt.Errorf("Cannot open file %s: %w", fname, err)
+		}
+
+		// Parse the CSV into a slice of float64 numbers
+		data, err := csv2float(f, column)
+		if err != nil {
+			return err
+		}
+
+		if err := f.Close(); err != nil {
+			return err
+		}
+
+		// Append the data to consolidate
+		consolidate = append(consolidate, data...)
+	}
+
+	_, err := fmt.Fprintln(out, opFunc(consolidate))
+	return err
 }
